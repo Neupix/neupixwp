d4487673f961b33c48b4f72fcb6e016ad03af90c
javierbyte <hola@javierbyte.com>
2014-04-21 13:45:43 -0600

    re added some files

--- functions.php
+++ functions.php
@@ -91,3 +91,13 @@ function neupix_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'neupix_scripts' );
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
